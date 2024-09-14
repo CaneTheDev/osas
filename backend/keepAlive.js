e9053892ee3d7600ff5eea7d73b522f871379c7c
tester5055 <olowookereolami2@gmail.com>
2024-09-14 19:26:41 +0100

    Update keepAlive.js

--- backend/keepAlive.js
+++ backend/keepAlive.js
@@ -1,10 +1,15 @@
 const https = require('https');  // Use 'https' module for https URLs
 
-// Function to ping the server every 1 minute
+// Function to ping the servers every 1 minute
 function keepAlive() {
     setInterval(() => {
-        https.get('https://osas-avw7.onrender.com/'); // Render URL with https
-        console.log('Pinged the server to keep it awake');
+        // Ping the first server
+        https.get('https://osas-avw7.onrender.com/');
+        console.log('Pinged osas-avw7 server to keep it awake');
+
+        // Ping the second server
+        https.get('https://oyaa.onrender.com/');
+        console.log('Pinged oyaa server to keep it awake');
     }, 60000); // 60000ms = 1 minute
 }
 
